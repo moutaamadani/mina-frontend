332f471e9d870c8157d7095a04e4f6f74efaccf5
moutaamadani <madanimoutaa.visions@gmail.com>
2025-12-17 20:49:00 +0400

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@
 import React from "react";
 import { AuthGate } from "./components/AuthGate";
 import MinaApp from "./MinaApp";
-import AdminDashboard from "./AdminDashboard";
 
 export default function App() {
-  const pathname = typeof window !== "undefined" ? window.location.pathname : "/";
-
-  // support /admin and /admin/anything
-  const isAdminPath = pathname === "/admin" || pathname.startsWith("/admin/");
-
-  return <AuthGate>{isAdminPath ? <AdminDashboard /> : <MinaApp />}</AuthGate>;
+  return (
+    <AuthGate>
+      <MinaApp />
+    </AuthGate>
+  );
 }
