589c51f01a72be41fc30ccfda2125984637b79ea
moutaamadani <madanimoutaa.visions@gmail.com>
2026-02-27 09:15:27 +0400

    Update MatchaQtyModal.tsx

--- src/components/MatchaQtyModal.tsx
+++ src/components/MatchaQtyModal.tsx
@@ -46,7 +46,7 @@ const MatchaQtyModal: React.FC<Props> = ({
   onConfirm,
 
   subtitle = "Mina uses matchas to create and animate your images.",
-  rulesLine = `1 niche image or 1s Video = 2 Matchas`,
+  rulesLine = `1 Niche Image or 1s Video = 2 Matchas`,
 
   baseCredits = 50,
   basePrice = 15,
